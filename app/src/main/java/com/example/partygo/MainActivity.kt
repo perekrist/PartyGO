690d7a5cc6ad2a7dbb910b8df8bbbb088c36c5af
Kristina Peregudova <perekrist31@gmail.com>
2019-12-14 13:09:08 +0700

    update template

--- app/src/main/java/com/example/partygo/MainActivity.kt
+++ app/src/main/java/com/example/partygo/MainActivity.kt
@@ -20,10 +20,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_chat, R.id.navigation_ticket, R.id.navigation_account
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
