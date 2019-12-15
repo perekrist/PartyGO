ceaae7dcc83a6e77774a68cec7c92948c41c8ad7
Kristina Peregudova <perekrist31@gmail.com>
2019-12-15 09:48:17 +0700

    marks add correcrly

--- app/src/main/java/com/example/partygo/Event.kt
+++ app/src/main/java/com/example/partygo/Event.kt
@@ -8,7 +8,7 @@ class Event(
 )
 
 var events = arrayListOf(
-    Event("LHD", R.drawable.lhd, 56.46, 84.95),
-    Event("X-mas party", R.drawable.xmas, 56.46, 84.94),
-    Event("HNY", R.drawable.hny, 56.46, 84.95)
+    Event("LHD", R.drawable.lhd, 56.463591, 84.957177),
+    Event("X-mas party", R.drawable.xmas, 56.4695, 84.94751300000007),
+    Event("HNY", R.drawable.hny, 56.461462, 84.948264)
 )
--- app/src/main/java/com/example/partygo/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/partygo/ui/search/SearchFragment.kt
@@ -4,38 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.partygo.R
 import com.example.partygo.events
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_search.*
 
-class SearchFragment : Fragment(), OnMapReadyCallback,  GoogleMap.OnMarkerClickListener {
+
+class SearchFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
     override fun onMarkerClick(p0: Marker?): Boolean {
-        return false
+        return true
     }
 
     private lateinit var mMap: GoogleMap
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val place = LatLng(40.73, -73.99)
-        mMap.addMarker(MarkerOptions().position(place))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.0f))
-
-        println("hello")
-        for (i in events) {
-            addMark(i.lat, i.lng)
-            println(i.lat)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,13 +28,32 @@ class SearchFragment : Fragment(), OnMapReadyCallback,  GoogleMap.OnMarkerClickL
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_search, container, false)
-
         return root
     }
 
-    private fun addMark(lat: Double, lng: Double) {
-        val place = LatLng(lat, lng)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapView = view.findViewById(R.id.map) as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val place = LatLng(40.73, -73.99)
         mMap.addMarker(MarkerOptions().position(place))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.0f))
+
+        for (i in events) {
+            addMark(i.lat, i.lng, i.name)
+        }
+    }
+
+    private fun addMark(lat: Double, lng: Double, title: String) {
+        val place = LatLng(lat, lng)
+        mMap.addMarker(MarkerOptions().position(place).title(title))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.0f))
 
     }
