975f84ea47973bba18e53a81eff8919c3338ad6d
Kristina Peregudova <perekrist31@gmail.com>
2019-12-15 11:01:07 +0700

    map searching

--- app/src/main/java/com/example/partygo/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/partygo/ui/search/SearchFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.partygo.R
 import com.example.partygo.events
@@ -17,7 +19,6 @@ import kotlinx.android.synthetic.main.fragment_search.*
 class SearchFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     override fun onMarkerClick(p0: Marker?): Boolean {
-        println(p0)
         return true
     }
 
@@ -39,18 +40,42 @@ class SearchFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
         mapView.onCreate(savedInstanceState)
         mapView.onResume()
         mapView.getMapAsync(this)
+
+        search_bar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String): Boolean {
+                mMap.clear()
+                val regex = Regex(query)
+                val currentEvents = events.filter {
+                    it.name.contains(regex)
+                }
+                for (i in currentEvents){
+                    addMark(i.lat, i.lng, i.name)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                mMap.clear()
+                val regex = Regex(query)
+                val currentEvents = events.filter {
+                    it.name.contains(regex)
+                }
+                for (i in currentEvents){
+                    addMark(i.lat, i.lng, i.name)
+                }
+                return true
+            }
+        })
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val place = LatLng(40.73, -73.99)
-        mMap.addMarker(MarkerOptions().position(place))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.0f))
-
         for (i in events) {
             addMark(i.lat, i.lng, i.name)
         }
     }
+
+
 
     private fun addMark(lat: Double, lng: Double, title: String) {
         val place = LatLng(lat, lng)
